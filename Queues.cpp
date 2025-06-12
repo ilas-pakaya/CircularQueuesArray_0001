74f53a294c53e0207aa822caa535ef495991fdb7
ilas-pakaya <ikhlaspakaya73@gmail.com>
2025-06-12 10:12:32 +0700

    cek antrian kosong

--- Queues.cpp
+++ Queues.cpp
@@ -16,10 +16,14 @@ public:
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
-    }
 
     if ((FRONT == 0 && REAR == max - 1) || (REAR == (FRONT - 1) % (max - 1))) {
         cout << "\nQueue overflow\n";
         return;
     }
+
+    if (FRONT == -1) {
+        FRONT = 0;
+        REAR = 0;
+    } 
 }
