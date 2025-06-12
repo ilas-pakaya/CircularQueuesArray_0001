69d41707e8aad1cf6491727ffdbb9b4b06eb9444
ilas-pakaya <ikhlaspakaya73@gmail.com>
2025-06-12 10:27:30 +0700

    jika elemen yg di hapus berada di posisi akhir array

--- Queues.cpp
+++ Queues.cpp
@@ -41,5 +41,17 @@ public:
             cout << "\nQueue underflow\n";
             return;
         }       
+        cout << "\nElement terhapus: " << queue_array[FRONT] << "\n";
+        
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;  
+        }
+        else {
+            if (FRONT == max - 1)
+                FRONT = 0; 
+            else
+                FRONT = FRONT + 1;
+        }
     }
 }
